d06d464d3985dc7512634099f84c94a61deb940a
dmerci1 <danmercia@gmail.com>
2018-06-13 19:47:07 -0400

    auth server is up and running

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,5 +1,15 @@
+const jwt = require('jwt-simple');
+const config = require('../config');
 const User = require('../models/user');
 
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode( {sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+  res.send({ token: tokenForUser(req.user) });
+}
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
@@ -23,7 +33,7 @@ exports.signup = function(req, res, next) {
       user.save(function(err) {
         if (err) { return next(err); }
 
-        res.json({ success: true });
+        res.json({ token: tokenForUser(user) });
       });
   });
 }
